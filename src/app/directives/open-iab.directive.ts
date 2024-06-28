76fc5c14081d86a91d03c875f3b1c3cab7fa5102
Eduardo Roth <eduardo@eduardoroth.dev>
2024-06-28 12:37:59 -0600

    feat(): final version pending files

---
+++ src/app/directives/open-iab.directive.ts
@@ -0,0 +1,32 @@
+import {
+  afterNextRender,
+  Directive,
+  ElementRef,
+  HostListener,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { Browser } from '@capacitor/browser';
+import { Device } from '@capacitor/device';
+
+@Directive({
+  selector: 'a[href]',
+  standalone: true,
+})
+export class OpenIabDirective {
+  private readonly el = inject(ElementRef);
+
+  @HostListener('click', ['$event'])
+  async onClick(event: PointerEvent) {
+    event.preventDefault();
+    const { platform } = await Device.getInfo();
+    if (platform !== 'web') {
+      await Browser.open({
+        url: this.el.nativeElement.href,
+        presentationStyle: 'popover',
+      });
+    } else {
+      document.location.href = this.el.nativeElement.href;
+    }
+  }
+}
